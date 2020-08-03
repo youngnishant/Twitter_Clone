2f70ee4ed012aadd9795579aee995cd050040af0
Nishant Sharma <youngnishant@outlook.com>
2020-08-03 18:06:17 +0530

    updated

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
-import Nav from '../components/nav'
 
 export default function IndexPage() {
   return (
     <div>
-      <Nav />
       <div className="py-20">
         <h1 className="text-5xl text-center text-accent-1">
-          Next.js + Tailwind CSS
+          Create-Tailwind-NextJS-App
         </h1>
       </div>
     </div>
